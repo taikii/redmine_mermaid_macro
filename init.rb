049c2d9cf0070c78a129ca548f8b88c76699b93a
taikii <taikii.git@outlook.com>
2023-03-26 10:31:59 +0900

    for mermaid v10

--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ Redmine::Plugin.register :redmine_mermaid_macro do
   name 'Redmine Mermaid Macro plugin'
   author 'Taiki IKEGAME'
   description 'Add mermaid graphs to your wiki.'
-  version '1.0.5'
+  version '1.1.0'
   url 'https://github.com/taikii/redmine_mermaid_macro'
   author_url 'https://taikii.github.io'
 
-  settings :default => { 'mermaid_url' => 'https://cdn.jsdelivr.net/npm/mermaid@9/dist/mermaid.js' },
+  settings :default => { 'mermaid_url' => 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs' },
            :partial => 'settings/redmine_mermaid_macro_settings'
 
   Redmine::WikiFormatting::Macros.register do
@@ -22,9 +22,15 @@ Redmine::Plugin.register :redmine_mermaid_macro do
          "}}"
     macro :mermaid do |obj, args, text|
       divid = "mermaid_" + SecureRandom.urlsafe_base64(8)
+
+      js = ""
+      js << "import mermaid from 'mermaid';\n"
+      js << "mermaid.initialize({ startOnLoad: false });\n"
+      js << "mermaid.run({querySelector: '##{divid}'});\n"
+
       out = ''.html_safe
       out << content_tag(:div, text, id: divid, class: 'mermaid')
-      out << javascript_tag("if (typeof mermaidInitialized != 'undefined') {mermaid.init(undefined, '#" + divid + "');};")
+      out << javascript_tag(js, type: 'module')
       out
     end
   end
--- lib/mermaid_macro_hook.rb
+++ lib/mermaid_macro_hook.rb
@@ -8,11 +8,19 @@ class MermaidMacroHook < Redmine::Hook::ViewListener
     return html
   end
 
-  def view_layouts_base_body_bottom(context={})
-    html = ""
-    html << javascript_include_tag(Setting.plugin_redmine_mermaid_macro['mermaid_url'])
-    html << javascript_tag("mermaid.initialize({startOnLoad:true});var mermaidInitialized = 1;")
-    return html
+  def view_layouts_base_html_head(context={})
+    # js = ""
+    # js << "import mermaid from '#{Setting.plugin_redmine_mermaid_macro['mermaid_url']}';"
+    # js << "mermaid.initialize({startOnLoad:true});"
+    # return javascript_tag(js, type: 'module').html_safe
+    
+    js = ""
+    js << "{"
+    js << "\"imports\": {"
+    js << "\"mermaid\": \"#{Setting.plugin_redmine_mermaid_macro['mermaid_url']}\""
+    js << "}"
+    js << "}"
+    return javascript_tag(js, type: 'importmap').html_safe
   end
 
 end
